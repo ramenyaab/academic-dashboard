c67fa48cab44e5eb31695d2df049b2ade593b180
ramenyaab <abdulyahaya6008@gmail.com>
2026-04-14 00:45:31 +0100

    Create script.js with extracted JavaScript from index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript extracted from index.html
+// Add your JavaScript code here.
+
+// Example function 
+function exampleFunction() {
+    console.log('This is an example function.');
+}
+
+// You can include more functions or scripts that were in index.html.
